51822e0d05ea4cec6f34f44c608c242ceb32e5ee
Oluwatobi Akinsanya <o.m.akinsanya@gmail.com>
2020-09-14 02:14:23 -0400

    API Homework

---
+++ VacationPy/api_key.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "50fd74fc1b7df8131e6adcf79472037e"
+
+# Google API Key
+g_key = "AIzaSyBvEcFZun7c8dCoSeRJemGLu0lKeTCPhfc"
---
+++ WeatherPy/api_key.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "2bcd14c647c59d11787fe15351a3ae02"
+
+# Google API Key
+g_key = "AIzaSyBvEcFZun7c8dCoSeRJemGLu0lKeTCPhfc"
